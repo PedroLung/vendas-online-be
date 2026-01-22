6affe6eab9ce1381c66ee1e5b0ada4f2ff66e187
PedroLung <pguedeslsilva@gmail.com>
2026-01-22 08:19:40 -0300

    feat: create connection with database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,24 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { UserModule } from './user/user.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [UserModule],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: ['.env.development.local'],
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      database: process.env.DB_DATABASE,
+      host: process.env.DB_HOST,
+      password: process.env.DB_PASSWORD,
+      port: Number(process.env.DB_PORT), // como a porta espera um inteiro e o env sempre retorna uma string é necessário colocar o Number()
+      username: process.env.DB_USERNAME,
+    }),
+    UserModule,
+  ],
+
   controllers: [],
   providers: [],
 })
