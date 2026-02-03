7e966fe6f052c02e85af935a4ecbf313997ed773
PedroLung <pguedeslsilva@gmail.com>
2026-02-03 12:20:13 -0300

    feat: create address controller

--- src/migration/1769277157657-create_table_address.ts
+++ src/migration/1769277157657-create_table_address.ts
@@ -1,4 +1,9 @@
-import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+import {
+  MigrationInterface,
+  QueryRunner,
+  Table,
+  TableForeignKey,
+} from 'typeorm';
 
 export class CreateTableAddress1769277157657 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
@@ -13,16 +18,81 @@ export class CreateTableAddress1769277157657 implements MigrationInterface {
             isGenerated: true,
             generationStrategy: 'increment',
           },
-          { name: 'name', type: 'varchar', isNullable: false },
-          { name: 'created_at', type: 'timestamp', default: 'now()' },
-          { name: 'updated_at', type: 'timestamp', default: 'now()' },
-          { name: 'deleted_at', type: 'timestamp', isNullable: true },
+          {
+            name: 'user_id',
+            type: 'int',
+            isNullable: false,
+          },
+          {
+            name: 'complement',
+            type: 'varchar',
+            isNullable: true,
+          },
+          {
+            name: 'number',
+            type: 'int',
+            isNullable: false,
+          },
+          {
+            name: 'cep',
+            type: 'varchar',
+            isNullable: false,
+          },
+          {
+            name: 'city_id',
+            type: 'int',
+            isNullable: false,
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()',
+            isNullable: false,
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp',
+            default: 'now()',
+            isNullable: false,
+          },
+          {
+            name: 'deleted_at',
+            type: 'timestamp',
+            isNullable: true,
+          },
         ],
+      }),
+    );
+
+    // FK para user_id
+    await queryRunner.createForeignKey(
+      'address',
+      new TableForeignKey({
+        name: 'FK_address_user_id',
+        columnNames: ['user_id'],
+        referencedTableName: 'users',
+        referencedColumnNames: ['id'],
+        onDelete: 'CASCADE',
+        onUpdate: 'CASCADE',
+      }),
+    );
+
+    await queryRunner.createForeignKey(
+      'address',
+      new TableForeignKey({
+        name: 'FK_address_city_id',
+        columnNames: ['city_id'],
+        referencedTableName: 'city',
+        referencedColumnNames: ['id'],
+        onDelete: 'CASCADE',
+        onUpdate: 'CASCADE',
       }),
     );
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropForeignKey('address', 'FK_address_city_id');
+    await queryRunner.dropForeignKey('address', 'FK_address_user_id');
     await queryRunner.dropTable('address');
   }
 }
