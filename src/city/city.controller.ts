2dfa3f13de78de081b88fc9b082cef60ae384af5
PedroLung <pguedeslsilva@gmail.com>
2026-02-03 08:56:48 -0300

    feat: create city controller and insert cache

--- src/city/city.controller.ts
+++ src/city/city.controller.ts
@@ -1,4 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Param } from '@nestjs/common';
+import { CityEntity } from './entity/city.entity';
+import { CityService } from './city.service';
+import { Get } from '@nestjs/common';
 
 @Controller('city')
-export class CityController {}
+export class CityController {
+  constructor(private readonly cityService: CityService) {}
+
+  @Get('/:stateId')
+  async getAllCitiesByStateId(
+    @Param('stateId') stateId: number,
+  ): Promise<CityEntity[]> {
+    return this.cityService.getAllCitiesByStateId(stateId);
+  }
+}
--- src/city/city.module.ts
+++ src/city/city.module.ts
@@ -1,8 +1,17 @@
 import { Module } from '@nestjs/common';
 import { CityController } from './city.controller';
 import { CityService } from './city.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { CityEntity } from './entity/city.entity';
+import { CacheModule } from '@nestjs/cache-manager';
 
 @Module({
+  imports: [
+    CacheModule.register({
+      ttl: 900000000,
+    }),
+    TypeOrmModule.forFeature([CityEntity]),
+  ],
   controllers: [CityController],
   providers: [CityService],
 })
